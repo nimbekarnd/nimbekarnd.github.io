d534c1033a00d68d7b4e0eaad3dc6ed425b8d714
Nupur Nimbekar <nimbekarnupur@gmail.com>
2026-01-29 17:16:38 -0500

    [Phase 1] Test visualization

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,9 @@ fetch(`https://api.github.com/users/${RESUME_CONFIG.githubUsername}/repos?per_pa
 .then(repos=>{
   const featured = document.querySelector("[data-featured-repos]");
   const pinned = document.querySelector("[data-pinned-repos]");
+  const more_repo = document.querySelector("[data-more-repos]");
   if(featured){
     featured.innerHTML = repos.slice(0,6).map(r=>`
-<<<<<<< HEAD
-=======
       <a class="card" href="${r.html_url}" target="_blank">
         <strong>${r.name}</strong><br/>
         <small>${r.description||""}</small>
@@ -16,7 +15,14 @@ fetch(`https://api.github.com/users/${RESUME_CONFIG.githubUsername}/repos?per_pa
 
   if(pinned){
     pinned.innerHTML = repos.slice(0,6).map(r=>`
->>>>>>> b9e3745 ([Phase 1] Test visualization)
+      <a class="card" href="${r.html_url}" target="_blank">
+        <strong>${r.name}</strong><br/>
+        <small>${r.description||""}</small>
+      </a>`).join("");
+  }
+
+  if(more_repo){
+    more_repo.innerHTML = repos.slice(0,6).map(r=>`
       <a class="card" href="${r.html_url}" target="_blank">
         <strong>${r.name}</strong><br/>
         <small>${r.description||""}</small>
